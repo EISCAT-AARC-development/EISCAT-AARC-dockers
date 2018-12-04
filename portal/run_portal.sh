ec13494bc3a4186be4a1e8f238ebc59cc3a52789
Niels van Dijk <niels.vandijk@surfnet.nl>
2018-12-04 16:25:27 +0100

    Updated dockers to include lates federated setup

--- portal/run_portal.sh
+++ portal/run_portal.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-IMAGE_TAG=eiscat-aarc/portal:v4
+TYPE=dev
+IMAGE_TAG=eiscat-aarc/portal:v6
 CONTAINER_NAME=eiscat-aarc-portal
 
 # Build the docker image if needed
@@ -7,20 +8,14 @@ if [[ "$(docker images -q $IMAGE_TAG 2> /dev/null)" == "" ]]; then
   docker build -t $IMAGE_TAG .
 fi
 
-# Pull relevant git repos iff they do not exist yet
-#git clone https://www.github.com/EISCAT-AARC-development/schedule.git $PWD/workdir//htdocs/schedule  # /var/www/html/htdocs/schedule
-#git clone https://www.github.com/EISCAT-AARC-development/tape_db.git $PWD/workdir/tape_db #/var/www/html/tape_db
-
 # find the location of configs in current directory structure
 RUN_DIR=$PWD
 CONFIG_DIR="$RUN_DIR/config"
 
+
 # Start data portal and server
 #docker start $CONTAINER_NAME || \
     docker run -it \
-	   --volume $PWD/workdir:/var/www/html \
-           --volume $PWD/authdir:/var/www/auth \
-	   --volume $PWD/archive:/data/archive \
 	   --env DATA_DIR=/var/portal \
 	   --net eiscat-aarc.local \
 	   --ip 192.168.111.100 \
@@ -29,3 +24,8 @@ CONFIG_DIR="$RUN_DIR/config"
 	   --publish 8443:443 \
 	   --publish 37009:37009 \
 	   $IMAGE_TAG
+
+#	   --volume $PWD/workdir:/var/www/html \
+#           --volume $PWD/authdir:/var/www/auth \
+#	   --volume $PWD/archive:/data/archive \
+
